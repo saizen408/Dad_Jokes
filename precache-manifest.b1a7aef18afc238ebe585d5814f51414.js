d407cb2e64105d0aa6ed8bb3fb1841bdcda345cd
Eric Owusu <llkoole7@gmail.com>
2019-12-07 21:00:32 -0800

    Updates

---
+++ precache-manifest.b1a7aef18afc238ebe585d5814f51414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc2dbfa46be252966da2e68667935a5",
+    "url": "/Dad_Jokes/index.html"
+  },
+  {
+    "revision": "ed7cb6efc8dc8c967ac3",
+    "url": "/Dad_Jokes/static/css/main.c91ebf25.chunk.css"
+  },
+  {
+    "revision": "fc8486ef70ba21f1fefb",
+    "url": "/Dad_Jokes/static/js/2.fb1c7897.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Dad_Jokes/static/js/2.fb1c7897.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed7cb6efc8dc8c967ac3",
+    "url": "/Dad_Jokes/static/js/main.a15d1a86.chunk.js"
+  },
+  {
+    "revision": "8382991fc0f0dfe26fd5",
+    "url": "/Dad_Jokes/static/js/runtime-main.68eba533.js"
+  }
+]);
